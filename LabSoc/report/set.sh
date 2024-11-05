84adb615e10e24c1ff30ee1e951e97caf499f5e5
Li2_Prime <litolagunday@gmail.com>
2024-11-05 11:16:21 +0800

    Update set.sh

--- LabSoc/report/set.sh
+++ LabSoc/report/set.sh
@@ -5,5 +5,10 @@ mv ~/.cache/nvim ~/.cache/nvim.bak
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
 # remove template's git connection to set up your own later
 rm -rf ~/.config/nvim/.git
-nvim
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install glow
+
 
